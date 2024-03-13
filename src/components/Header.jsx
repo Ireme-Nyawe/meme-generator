4b880533d69eab26b1ac58c1bdb4969f559fac59
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 18:10:09 +0200

    design header-component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <nav className="nav">
+      <div className="part-1">
+        <img src="../src/assets/meme-logo.png" alt="meme-logo" className="meme-logo"/>
+        <h4>MemeGenerator</h4>
+      </div>
+      <span>React Course - Project 3</span>
+    </nav>
+  );
+}
