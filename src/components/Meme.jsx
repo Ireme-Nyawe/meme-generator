4f0fc05f9b29c9e5aac72ef37d0bfcd3d303641b
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 18:26:00 +0200

    add meme component with 2 input and button

---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme(){
+    return (
+        <div className="meme-component">
+            <div className="meme-inputs">
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <button className="meme-button">Get a new meme image  🖼</button>
+        </div>
+    )
+}
