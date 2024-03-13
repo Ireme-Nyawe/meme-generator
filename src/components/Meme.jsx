2bc642b3eb5332ee228204064597e36c37f48897
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 21:55:14 +0200

    apply hooks to change image randomly

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,34 @@
-export default function Meme(){
-    return (
-        <div className="meme-component">
-            <div className="meme-inputs">
-                <input type="text" />
-                <input type="text" />
-            </div>
-            <button className="meme-button">Get a new meme image  🖼</button>
+import { useState } from "react";
+import MemeData from "./MemeData";
+export default function Meme() {
+  let memeUrl;
+  const memesArray = MemeData;
+  let randomNumber = Math.floor(Math.random() * memesArray.length);
+  memeUrl = memesArray[randomNumber].imageUrl;
+  const [memeImage, setImage] = useState(memeUrl);
+  function handleClick() {
+    randomNumber = Math.floor(Math.random() * memesArray.length);
+    memeUrl = memesArray[randomNumber].imageUrl;
+    setImage(memeUrl);
+  }
+  return (
+    <div className="meme-component">
+      <div className="meme-inputs">
+        <div className="input-labeled">
+          <label>Top Text</label>
+          <input type="text" />
         </div>
-    )
+        <div className="input-labeled">
+          <label>Bottom text</label>
+          <input type="text" />
+        </div>
+      </div>
+      <button onClick={handleClick} className="meme-button">
+        Get a new meme image 🖼
+      </button>
+      <div className="">
+        <img src={memeUrl} alt="Meme-image" className="meme-image" />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/MemeData.jsx
@@ -0,0 +1,46 @@
+export default [
+  {
+    title: "Doge",
+    location: "Internet",
+    googleMapLink: "https://www.google.com/maps/place/Internet/",
+    startDate: "2013-12-06",
+    endDate: "Present",
+    description:
+      "A popular internet meme featuring a Shiba Inu dog accompanied by multicolored text in Comic Sans font.",
+    imageUrl:
+      "https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg",
+  },
+  {
+    title: "Grumpy Cat",
+    location: "Internet",
+    googleMapLink: "https://www.google.com/maps/place/Internet/",
+    startDate: "2012-09-22",
+    endDate: "2019-05-14",
+    description:
+      "A famous internet meme featuring a cat named Tardar Sauce with a distinctive facial expression.",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyLc8LMQIrviNz6u2EP3TtPRZZt3OlhrhHA&usqp=CAU",
+  },
+  {
+    title: "Rickrolling",
+    location: "Internet",
+    googleMapLink: "https://www.google.com/maps/place/Internet/",
+    startDate: "2007-05-15",
+    endDate: "Present",
+    description:
+      "An internet prank that involves misleading links redirecting users to Rick Astley's song 'Never Gonna Give You Up.'",
+    imageUrl:
+      "https://www.adobe.com/express/create/media_10d832fed2b86d900503c50ac4b30b14058e4134c.jpeg?width=400&format=jpeg&optimize=medium",
+  },
+  {
+    title: "Doge",
+    location: "Internet",
+    googleMapLink: "https://www.google.com/maps/place/Internet/",
+    startDate: "2013-12-06",
+    endDate: "Present",
+    description:
+      "A popular internet meme featuring a Shiba Inu dog accompanied by multicolored text in Comic Sans font.",
+    imageUrl:
+      "https://www.rollingstone.com/wp-content/uploads/2023/12/Skibidi-Toilet-meme.jpg?w=800",
+  },
+];
