c86d6bc0e1593e4463d6e40ecaa616bbf7abc055
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-16 14:24:24 +0200

    use state to ensure each keyup in meme text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -5,20 +5,26 @@ export default function Meme() {
   const memesArray = MemeData;
   let randomNumber = Math.floor(Math.random() * memesArray.length);
   memeUrl = memesArray[randomNumber].imageUrl;
-    const [meme, setMeme] = useState({
+  const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     memeImage:
       "https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg",
   });
-  const [allMemeImage]= useState(MemeData);
+
+  const [memeText, setMemeText] = useState({ topText: "", bottomText: "" });
+  function handleChange(event) {
+    setMemeText((prevMemeText) => {
+      return { ...prevMemeText, [event.target.name]: event.target.value };
+    });
+  }
 
   function handleClick() {
     randomNumber = Math.floor(Math.random() * memesArray.length);
     memeUrl = memesArray[randomNumber].imageUrl;
-    setMeme(prevMeme=>({
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      memeImage:memeUrl
+      memeImage: memeUrl,
     }));
   }
   return (
@@ -26,21 +32,33 @@ export default function Meme() {
       <div className="meme-inputs">
         <div className="input-labeled">
           <label>Top Text</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="topText"
+            value={memeText.topText}
+            placeholder="Type---"
+            onChange={handleChange}
+          />
         </div>
         <div className="input-labeled">
           <label>Bottom text</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="bottomText"
+            value={memeText.bottomText}
+            placeholder="Type---"
+            onChange={handleChange}
+          />
         </div>
       </div>
       <button onClick={handleClick} className="meme-button">
         Get a new meme image 🖼
       </button>
-    <div className="meme">
+      <div className="meme">
         <img src={meme.memeImage} alt="Meme-image" className="meme-image" />
-        <h2 className="top-text">Top-text</h2>
-        <h2 className="bottom-text">Bottom-Text</h2>
+        <h2 className="top-text">{memeText.topText}</h2>
+        <h2 className="bottom-text">{memeText.bottomText}</h2>
+      </div>
     </div>
-</div>
   );
 }
